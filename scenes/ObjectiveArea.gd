b1d7a7fb1c1eb6616b9be2d088401db23cee53c9
DarilRaf <darilmrafansyah@gmail.com>
2025-02-18 08:03:36 +0700

    Latihan Mandiri Done

--- scenes/ObjectiveArea.gd
+++ scenes/ObjectiveArea.gd
@@ -1,5 +1,27 @@
 extends Area2D
 
+@export var done: TextureRect
+var SECOND_LEVEL = load("res://scenes/SecondLevel.tscn")
+var MAIN_LEVEL = load("res://scenes/MainLevel.tscn")
+
 func _on_ObjectiveArea_body_entered(body: RigidBody2D):
-	if (body.name == "BlueShip"):
+	if (body.is_in_group("ship")):
 		print("Reached objective!")
+		if done:
+			done.visible = true
+
+func _on_body_entered(body: Node2D) -> void:
+	#if (body.is_in_group("ship")):
+	print("Fall!")
+	get_tree().reload_current_scene()
+
+func _on_button_pressed() -> void:
+	print("Loading next level...")
+	# Get the name of the currently active scene
+	var current_scene = get_tree().current_scene.scene_file_path
+
+	# Decide which scene to load next
+	if current_scene == "res://scenes/MainLevel.tscn":
+		get_tree().change_scene_to_packed(SECOND_LEVEL)  # Load SecondLevel
+	else:
+		get_tree().change_scene_to_packed(MAIN_LEVEL)  # Go back to MainLevel
--- scenes/PlatformBlue.gd
+++ scenes/PlatformBlue.gd
@@ -2,14 +2,27 @@ extends Node2D
 
 @export var speed = 500.0
 
+var movement_enabled = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Platform initialized")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("move_up"):
-		move_local_y(-speed * delta, false)
-	
-	if Input.is_action_pressed("move_down"):
-		move_local_y(speed * delta, false)
+	if movement_enabled:
+		if Input.is_action_pressed("move_up"):
+			move_local_y(-speed * delta, false)
+		
+		if Input.is_action_pressed("move_down"):
+			move_local_y(speed * delta, false)
+
+		if Input.is_action_pressed("move_right"):
+			move_local_x(speed * delta, false)
+
+		if Input.is_action_pressed("move_left"):
+			move_local_x(-speed * delta, false)
+
+
+func _on_objective_area_body_entered(body: Node2D) -> void:
+	movement_enabled = false
